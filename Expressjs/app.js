5181f56769926f0c90340607e9f1222196b3d17d
sushilsree98 <sushil.sree98@gmail.com>
2021-11-17 23:49:34 +0530

    Express middleware

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -1,8 +1,21 @@
-const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-const server = http.createServer(()=>{})
+app.use(bodyParser.urlencoded({extended:false}));
 
-server.listen(3000)
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input name="text" type="text"/><button type="submit">Submit</button></form>')
+})
+
+app.use('/product',(req, res)=>{
+    console.log(req.body)
+    res.redirect("/")
+})
+
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Hello from Nodejs</h1>')
+})
+
+app.listen(3000)

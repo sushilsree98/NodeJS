06d36e01c20610e3389074c78773732579fbbf0d
sushilsree98 <sushil.sree98@gmail.com>
2021-11-27 18:38:48 +0530

    SQL added

--- Expressjs/controllers/shop.js
+++ Expressjs/controllers/shop.js
@@ -2,34 +2,42 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
-    });
-  });
+    })
+  })
+  .catch(err=>{console.log(err)})
+    
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId)
+  .then(([product])=>{
     res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
+      pageTitle: 'Edit Product',
+      path: '/products',
+      product: product[0]
+    })
+  })
+  .catch(err=>{
+    console.log(err)
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
-    });
-  });
+    })
+  })
 };
 
 exports.getCart = (req, res, next) => {

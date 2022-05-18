e68da642dba824faedece71a807f07ee54dd9287
sushilsree98 <sushil.sree98@gmail.com>
2022-05-18 22:37:52 +0530

    Sessions and cookies

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,7 +19,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  secret:'my secret',
+  resave: false,
+  saveUninitialized:false
+}))
 app.use((req, res, next) => {
   User.findById('62712d4c0af5e89eb0beda01')
     .then(user => {
--- Expressjs/controllers/auth.js
+++ Expressjs/controllers/auth.js
@@ -1,19 +1,20 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1]
-
+  // const isLoggedIn = req
+  //   .get('Cookie')
+  //   .split(';')[1]
+  //   .trim()
+  //   .split('=')[1]
+  console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: true
   });
 };
 
 exports.postLogin = (req, res, next) => {
   // req.isLoggedIn = true
-  res.setHeader('Set-Cookie','isLoggedIn=true')
+  // res.setHeader('Set-Cookie','isLoggedIn=true')
+  req.session.isLoggedIn = true
   res.redirect('/')
 };
